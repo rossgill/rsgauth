fcbd14613870cfbc6d8c9fe697bc1c4980e4f02b
Ross Gill <ross.gill.10@gmail.com>
2023-04-08 22:49:36 -0500

    Initial Commit

---
+++ rsgauth/rsgauthApp.swift
@@ -0,0 +1,17 @@
+//
+//  rsgauthApp.swift
+//  rsgauth
+//
+//  Created by Ross Gill on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct rsgauthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
